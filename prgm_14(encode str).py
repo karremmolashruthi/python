f9312c9027deb587c7a9b5270f2d6242d1899dac
karremmolashruthi <shruthi2713@gmail.com>
2024-07-30 21:27:58 +0530

    Day_1

---
+++ prgm_14(encode str).py
@@ -0,0 +1,12 @@
+n=input()
+res=""
+for i in n: #srt does not contain range
+    cur_num=int(i)
+    res+=str(cur_num**2)
+print(res)
+
+n=int(input())
+for i in range(n):
+    k=n**i
+print(k)    
+      
