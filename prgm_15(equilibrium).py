6d3ee051aaf84cb0c3a10cbdefb589da8e4af272
karremmolashruthi <shruthi2713@gmail.com>
2024-07-30 21:31:50 +0530

    Day_1

---
+++ prgm_15(equilibrium).py
@@ -0,0 +1,14 @@
+n=int(input())
+arr=list(map(int,input().split()))
+ans=0
+found=False
+for i in range(n):
+    left,right=sum(arr[:i]),sum(arr[i+1:])
+    if left==right:
+        found=True
+        ans=i+1
+        break
+if not found:
+    print("NOT FOUND")
+else:
+    print(ans)
