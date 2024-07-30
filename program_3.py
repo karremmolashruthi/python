300be474025b6567a9323ff246d48f60e98e8729
karremmolashruthi <shruthi2713@gmail.com>
2024-07-30 14:24:17 +0530

    Day_1

---
+++ program_3.py
@@ -0,0 +1,17 @@
+n=int(input())
+arr=list(map(int,input().split()))
+total_cho_a=0
+for num_of_cho in arr:
+      total_cho_a+=i//3
+      if i%3!=0:
+          total_cho_a+=1
+
+print(total_cho_a)      
+      
+      
+      
+      
+      
+      
+      
+      
