e666829e044408d28094146bb67aa77dfa416a6b
karremmolashruthi <shruthi2713@gmail.com>
2024-07-31 11:46:47 +0530

    Day_2

---
+++ prgm_17(alpha).py
@@ -0,0 +1,13 @@
+s1=set([1,2,3,4,5,6])
+s2=set([4,5,6,7,8,9])
+ans=s1.union(s2)
+ans1=s1.intersection(s2)
+ans3=s1.difference(s2)
+print(ans)
+print(ans1)
+print(ans3)
+
+s=set(input())
+alpha=set("abcdefghijklmnopqrstuvwxyz")
+diff=sorted(alpha.difference(s))
+print(''.join(diff))
